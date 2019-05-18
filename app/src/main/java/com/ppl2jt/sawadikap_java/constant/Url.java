fc8d5ba5e1d8b6dd814fd08f4abceb4785ada055
muhamistau <islam.taufikurahman@gmail.com>
2019-05-18 13:27:57 +0700

    change BASE Url

--- app/src/main/java/com/ppl2jt/sawadikap_java/constant/Url.java
+++ app/src/main/java/com/ppl2jt/sawadikap_java/constant/Url.java
@@ -3,7 +3,8 @@ package com.ppl2jt.sawadikap_java.constant;
 public class Url {
 
     // POST
-    public static final String BASE = "http://sawadikap-endpoint.herokuapp.com/api/";
+//    public static final String BASE = "http://sawadikap-endpoint.herokuapp.com/api/";
+    public static final String BASE = "http://sawadikap.himatif.org/api/";
     public static final String LOGIN = BASE + "login";
     public static final String SIGNUP = BASE + "signup";
     public static final String INPUT = BASE + "input";
